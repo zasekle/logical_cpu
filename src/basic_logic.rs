6b36a41f1164c11624c3e8101e3111c6ce98e5ad
Zasekle <quickshiftx@gmail.com>
2023-09-01 11:51:13 -0400

    Still working on the general design for how to write the circuits.

--- src/basic_logic.rs
+++ src/basic_logic.rs
@@ -1,3 +1,4 @@
+use std::rc::{Rc, Weak};
 use crate::basic_logic::Signal::{HIGH, LOW};
 
 #[derive(PartialEq, Debug, Clone)]
@@ -17,85 +18,209 @@ impl Signal {
             HIGH
         } else {
             LOW
-        }
-    }
-}
-
-pub trait LogicUnit {
-    fn input(&self, input: &[Signal]) -> Result<Vec<Signal>, LogicError>;
-}
-
-pub struct And;
-
-impl LogicUnit for And {
-    fn input(&self, input: &[Signal]) -> Result<Vec<Signal>, LogicError> {
-        if input.len() < 2 {
-            return Err(LogicError::InvalidInputSize);
-        }
-
-        let result = if input.iter().all(|i| *i == HIGH) {
-            HIGH
-        } else {
-            LOW
         };
-
-        Ok(vec![result])
     }
 }
 
-pub struct Or;
+//TODO: All input needs to be passed with the current clock_tick OR some kind of shared resource.
+struct Node {
+    connections: Vec<Rc<Node>>,
+    parent: Weak<LogicNodes>,
+    current_clock_tick: usize,
+    oscillations: usize,
+}
 
-impl LogicUnit for Or {
-    fn input(&self, input: &[Signal]) -> Result<Vec<Signal>, LogicError> {
-        if input.len() < 2 {
-            return Err(LogicError::InvalidInputSize);
+impl Node {
+    fn new(parent: Weak<LogicNodes>) -> Self {
+        Node {
+            connections: Vec::new(),
+            parent,
+            current_clock_tick: 0,
+            oscillations: 0,
         }
+    }
 
-        let result = if input.iter().any(|i| *i == HIGH) {
-            HIGH
-        } else {
-            LOW
-        };
+    //TODO: Need to crash if oscillations hits a certain point.
+}
 
-        Ok(vec![result])
+pub struct LogicNodes {
+    inputs: Vec<Box<Node>>,
+    outputs: Vec<Box<Node>>,
+    changed_count: usize,
+    input_size: usize,
+    output_size: usize,
+}
+
+impl LogicNodes {
+    fn new(input_size: usize, output_size: usize) -> Rc<Self> {
+        let inputs = Vec::with_capacity(input_size);
+        let outputs = Vec::with_capacity(output_size);
+
+        let mut self_reference = Rc::new(
+            LogicNodes {
+                inputs,
+                outputs,
+                changed_count: 0,
+                input_size,
+                output_size,
+            }
+        );
+
+        let weak_self_reference = Rc::downgrade(&self_reference);
+        let mut mut_self_reference = Rc::get_mut(&mut self_reference).unwrap();
+
+        mut_self_reference.inputs.resize_with(
+            input_size,
+            || Box::new(Node::new(Weak::clone(&weak_self_reference))),
+        );
+
+        mut_self_reference.outputs.resize_with(
+            output_size,
+            || Box::new(Node::new(Weak::clone(&weak_self_reference))),
+        );
+
+        self_reference
     }
 }
 
-pub struct Nand;
-
-impl LogicUnit for Nand {
-    fn input(&self, input: &[Signal]) -> Result<Vec<Signal>, LogicError> {
-        let and_gate = And{};
-        not_output(
-            &and_gate
-                .input(input)
-                .expect("Nand input was invalid")
-        )
-    }
+//TODO: Extract this to a separate struct and trait.
+//TODO: Maybe wrap this in something to eliminate the need to make a reference count every time. Or
+// maybe this is the wrapper for LogicNodes.
+pub struct And {
+    logic_nodes: Rc<LogicNodes>,
 }
 
-pub struct Nor;
+impl And {
+    //TODO: Need to store current state.
+    pub fn new(input_size: usize, output_size: usize) -> Self {
+        let mut logic_nodes = LogicNodes::new(input_size, output_size);
 
-impl LogicUnit for Nor {
-    fn input(&self, input: &[Signal]) -> Result<Vec<Signal>, LogicError> {
-        let or_gate = Or{};
-        not_output(
-            &or_gate
-                .input(input)
-                .expect("Nor input was invalid")
-        )
-    }
-}
-
-fn not_output(input: &[Signal]) -> Result<Vec<Signal>, LogicError> {
-    if input.is_empty() {
-        return Err(LogicError::InvalidInputSize);
+        And {
+           logic_nodes,
+        }
     }
 
-    Ok(
-        input
-            .iter()
-            .map(|s| Signal::swap(s) )
-            .collect()
-    )
+    //TODO: I would like for a single 'unit' to complete before moving on to another unit. So what
+    // does that even mean in practice? Is it even possible/practical to do? It seems that if I
+    // start at my clock, then iterate through each wire coming off it, then iterate through each
+    // 'next' one etc... would it be different doing breadth vs depth?
+    // breadth seems more reasonable, plus I may be able to multi-thread it eventually.
+
+    //TODO: What is the flow like for this system of doing it?
+    // 1) Signal goes in to a Node.
+    // 2) The Node calls the parent and runs whatever operation it needs.
+    // 3) Somehow the output is called and the next node is accessed.
+
+    //TODO: Will need to think about de-allocations. This is because the And stores a reference to
+    // Nodes and Node stores a reference to And.
 }
+
+// pub trait LogicUnit {
+//     fn input(&self, input: &[Signal]) -> Result<Vec<Signal>, LogicError>;
+// }
+//
+// pub struct And;
+//
+// impl LogicUnit for And {
+//     fn input(&self, input: &[Signal]) -> Result<Vec<Signal>, LogicError> {
+//         if input.len() < 2 {
+//             return Err(LogicError::InvalidInputSize);
+//         }
+//
+//         let result = if input.iter().all(|i| *i == HIGH) {
+//             HIGH
+//         } else {
+//             LOW
+//         };
+//
+//         Ok(vec![result])
+//     }
+// }
+//
+// pub struct Or;
+//
+// impl LogicUnit for Or {
+//     fn input(&self, input: &[Signal]) -> Result<Vec<Signal>, LogicError> {
+//         if input.len() < 2 {
+//             return Err(LogicError::InvalidInputSize);
+//         }
+//
+//         let result = if input.iter().any(|i| *i == HIGH) {
+//             HIGH
+//         } else {
+//             LOW
+//         };
+//
+//         Ok(vec![result])
+//     }
+// }
+//
+// pub struct Nand;
+//
+// impl LogicUnit for Nand {
+//     fn input(&self, input: &[Signal]) -> Result<Vec<Signal>, LogicError> {
+//         let and_gate = And{};
+//         not_output(
+//             &and_gate
+//                 .input(input)
+//                 .expect("Nand input was invalid")
+//         )
+//     }
+// }
+//
+// pub struct Nor;
+//
+// impl LogicUnit for Nor {
+//     fn input(&self, input: &[Signal]) -> Result<Vec<Signal>, LogicError> {
+//         let or_gate = Or{};
+//         not_output(
+//             &or_gate
+//                 .input(input)
+//                 .expect("Nor input was invalid")
+//         )
+//     }
+// }
+//
+// fn not_output(input: &[Signal]) -> Result<Vec<Signal>, LogicError> {
+//     if input.is_empty() {
+//         return Err(LogicError::InvalidInputSize);
+//     }
+//
+//     Ok(
+//         input
+//             .iter()
+//             .map(|s| Signal::swap(s) )
+//             .collect()
+//     )
+// }
+
+// use std::rc::{Rc, Weak};
+//
+// struct Parent {
+//     children: Vec<Node>,
+// }
+//
+// struct Node {
+//     parent: Weak<Parent>,
+// }
+//
+// impl Parent {
+//     fn new() -> Rc<Self> {
+//         Rc::new(Parent { children: vec![] })
+//     }
+//
+//     fn add_child(&mut self, parent_weak: Weak<Self>) {
+//         let child = Node {
+//             parent: Weak::clone(&parent_weak),
+//         };
+//         self.children.push(child);
+//     }
+// }
+//
+// fn main() {
+//     let mut parent = Parent::new();
+//     let parent_weak = Rc::downgrade(&parent);
+//     let mut parent_ref = Rc::get_mut(&mut parent).unwrap();
+//
+//     parent_ref.add_child(parent_weak);
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 mod basic_logic;
 
-use crate::basic_logic::{Signal::{HIGH, LOW}, And, Or, Nor, Nand, LogicUnit};
+// use crate::basic_logic::{Signal::{HIGH, LOW}, And, Or, Nor, Nand, LogicUnit};
 
 fn main() {
-    let and_gate = And{};
-    let or_gate = Or{};
-    let nor_gate = Nor{};
-    let nand_gate = Nand{};
-
-    let input = [HIGH, HIGH, HIGH, HIGH];
-    println!("and_gate: {:#?}", and_gate.input(&input).unwrap());
-
-    let input = [HIGH, HIGH];
-    println!("or_gate: {:#?}", or_gate.input(&input).unwrap());
-
-    let input = [HIGH, HIGH, LOW];
-    println!("nor_gate: {:#?}", nor_gate.input(&input).unwrap());
-
-    let input = [HIGH, HIGH];
-    println!("nand_gate: {:#?}", nand_gate.input(&input).unwrap());
+    // let and_gate = And {};
+    // let or_gate = Or {};
+    // let nor_gate = Nor {};
+    // let nand_gate = Nand {};
+    //
+    // let input = [HIGH, HIGH, HIGH, HIGH];
+    // println!("and_gate: {:#?}", and_gate.input(&input).unwrap());
+    //
+    // let input = [HIGH, HIGH];
+    // println!("or_gate: {:#?}", or_gate.input(&input).unwrap());
+    //
+    // let input = [HIGH, HIGH, LOW];
+    // println!("nor_gate: {:#?}", nor_gate.input(&input).unwrap());
+    //
+    // let input = [HIGH, HIGH];
+    // println!("nand_gate: {:#?}", nand_gate.input(&input).unwrap());
 
     //There is a bit of a problem with how I am thinking about things. That is the idea that state
     // itself can be considered here. For example, with the very basic circuit that is used in memory
@@ -27,7 +27,6 @@ fn main() {
 
     //Also with this model how would I simulate a gate attaching back to itself? Maybe Instead of
     // Signal inputs, I actually give them a copy of an object that represents input or output?
-
 
     //I could do a few things.
     // 1) Keep track of state.
@@ -39,26 +38,40 @@ fn main() {
     //   - I could have a 'connection' somehow. Say instead of just the logic gate, it also stores
     //     the units it connects to.
 
-
     //TODO: How would I eliminate loops? Maybe when I pass the connection through I assign each
     // logic unit a clock tick number. Then if the clock has already ticked ignore it. This could
     // cause other problems I assume. For example, if I propagate something out depth-wise maybe
     // when I increase the breadth, it will change these? Or maybe I should propagate based on the
     // unit, then go depth after that. This way
 
-
     //So my current idea is that, lets leave it the way it is. All solutions will have problems.
     // I will 'wire' it up manually inside larger and larger units. And I will add a clock tick
     // number to each unit.
 
     //TODO: How do I give it manual inputs? Maybe the clock works on a separate thread to the input
-    // and I just feed it commands from the GUI?
-
-    loop { //clock
-
-        let and_gate = And{};
+    // and I just feed it commands from the GUI? Maybe have the clock always running and it checks
+    // a vector for possible commands, then on the main thread here I input commands.
 
 
 
-    }
+
+    //I think that what I want is to add in two things to my gate, the first is to save current
+    // inputs. The second is to keep track of clock ticks. I think the big problem at the moment is
+    // that all inputs need to be put in at the same time. But there is no guarantee of that, also
+    // the clock tick thing may not be such a good idea. Say that I have an OR gate, I do some
+    // stuff and get a signal to it and update the clock tick. Then I get something else that goes
+    // to the other side of it, won't this NOT update? Maybe instead of the gate itself having one
+    // I have each little node have one? Is this more a flow problem?
+
+
+    //2 Situations
+    // NOT gate feeding back into itself (state will oscillate).
+    // OR gate feeding back into itself (on forever).
+
+
+    //So what I can do is measure the number of changes during a single clock cycle. Then if the
+    // number of changes gets above a certain number, I can just panic! or something. But the number
+    // must only be when the value CHANGES, not just when an update comes.
+
+
 }
